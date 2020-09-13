0c642454172180aaca9024c2d84c6b42550ad34e
fredrvaa <fredrvaa@stud.ntnu.no>
2020-09-14 01:28:34 +0200

    Just call visualizer class instead of storing it in variable

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--cell_size", type=int, default=16)
 args = parser.parse_args()
 
-visualizer = Visualizer(cell_size = args.cell_size)
+Visualizer(cell_size = args.cell_size)
