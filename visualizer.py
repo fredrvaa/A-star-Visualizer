843b678cd45625815692145f947eeeeef221fd4b
fredrvaa <fredrvaa@stud.ntnu.no>
2020-09-13 17:29:33 +0200

    Add basic user input to visualizer

--- visualizer.py
+++ visualizer.py
@@ -13,34 +13,65 @@ class Visualizer(object):
         pygame.display.set_caption("A* Visualizer")
         self.windowclock = pygame.time.Clock()
 
-        self.main()
+        self._main()
 
-    def main(self):
-        #Put all variables up here
-        stopped = False
+    def _main(self):
+        
+        running = True
+        drawing_weight = 1
 
-        while not stopped:
-             #Tuple for filling display... Current is white
-
+        while running:
             #Event Tasking
-            #Add all your event tasking things here
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    running = False
                     pygame.quit()
                     quit()
-                elif event.type == pygame.KEYDOWN:
-                    stopped = True
 
-            #Add things like player updates here
-            #Also things like score updates or drawing additional items
-            # Remember things on top get done first so they will update in the order yours is set at
-            self.draw()
-            # Remember to update your clock and display at the end
+                if event.type == pygame.KEYDOWN:
+                    # If 1,2,3,4 is pressed, the drawing weight is updated accordingly
+                    if pygame.key.name(event.key) == "1":
+                        drawing_weight = 1
+                    elif pygame.key.name(event.key) == "2":
+                        drawing_weight = 2
+                    elif pygame.key.name(event.key) == "3":
+                        drawing_weight = 3
+                    elif pygame.key.name(event.key) == "4":
+                        drawing_weight = 4
+                
+                # Draws weighted "STANDARD" cells
+                if pygame.mouse.get_pressed()[0]: # Left mouse click
+                    pos = pygame.mouse.get_pos()
+                    row, col = self._get_grid_pos(pos)
+                    self.map_obj.set_cell_state(row, col, "STANDARD", drawing_weight)
+                
+                # Draws "START" and "GOAL" cells
+                elif pygame.mouse.get_pressed()[1]: # Middle mouse click
+                    pos = pygame.mouse.get_pos()
+                    row, col = self._get_grid_pos(pos)
+                    cell = self.map_obj.get_cell(row, col)
+                    if self.map_obj.start_id is None and cell.state not in ["START", "GOAL"]:
+                        self.map_obj.set_cell_state(row, col, "START", cell.weight)
+                    elif self.map_obj.goal_id is None and cell.state not in ["START", "GOAL"]:
+                        self.map_obj.set_cell_state(row, col, "GOAL", cell.weight)
+
+                # DRAWS "BARRIER" cells
+                elif pygame.mouse.get_pressed()[2]: # Right mouse click
+                    # Gets cell from mouseclick
+                    pos = pygame.mouse.get_pos()
+                    row, col = self._get_grid_pos(pos)
+
+                    # Set cell state to barrier
+                    self.map_obj.set_cell_state(row, col, "BARRIER", -1)
+                
+            # Draw window
+            self._draw()
             
+            # Update display and clock
             pygame.display.update()
             self.windowclock.tick(60)
 
-    def draw(self):
+    def _draw(self):
         self.window.fill((255,255,255))
 
         # Draws all cells
@@ -63,9 +94,14 @@ class Visualizer(object):
             y1 = y2 = row * self.cell_size
             pygame.draw.line(self.window, (100,100,100), (x1,y1), (x2,y2))
 
+        # Draw legend
 
-    def get_clicked_pos(self, x, y):
-        pass
+
+    def _get_grid_pos(self, pos):
+        row = pos[1] // self.cell_size
+        col = pos[0] // self.cell_size
+        return row, col
+
 
 
 if __name__ == "__main__":
